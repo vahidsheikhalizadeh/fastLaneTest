319015a69d100abf957de2a53fce1f252abc25df
Vahid <71shva1mst@hft-stuttgart.de>
2018-10-18 15:51:44 +0200

    Initial Commit

---
+++ fastLaneTest/NewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewViewController.swift
+//  fastLaneTest
+//
+//  Created by Vahid Sheikhalizadeh on 10.10.18.
+//  Copyright © 2018 Vahid Sheikhalizadeh. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ fastLaneTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastLaneTest
+//
+//  Created by Vahid Sheikhalizadeh on 10.10.18.
+//  Copyright © 2018 Vahid Sheikhalizadeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
